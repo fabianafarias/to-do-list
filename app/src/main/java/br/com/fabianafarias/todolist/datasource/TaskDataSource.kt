200afed887636088d0a11915b56c9d7dce24e26e
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-01 21:42:01 -0300

    Utilizando TimePicker e criando tarefas com list e data class.

---
+++ app/src/main/java/br/com/fabianafarias/todolist/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package br.com.fabianafarias.todolist.datasource
+
+import br.com.fabianafarias.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task) {
+        list.add(task.copy(id = list.size + 1))
+    }
+}
