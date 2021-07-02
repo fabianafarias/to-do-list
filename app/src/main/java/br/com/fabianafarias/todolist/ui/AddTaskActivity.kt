3ce72dc515774eb58829cfc5035387f5e600ab1b
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-02 20:26:09 -0300

    Criando TaskListAdapter e toda a lógica do Recycler view até o botão criar

--- app/src/main/java/br/com/fabianafarias/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/br/com/fabianafarias/todolist/ui/AddTaskActivity.kt
@@ -61,7 +61,7 @@ class AddTaskActivity : AppCompatActivity() {
                 hour = binding.tilHour.text
             )
             TaskDataSource.insertTask(task)
-            Log.e("TAG", "insertListeners " + TaskDataSource.getList())
+            finish()
         }
     }
 
--- app/src/main/java/br/com/fabianafarias/todolist/ui/MainActivity.kt
+++ app/src/main/java/br/com/fabianafarias/todolist/ui/MainActivity.kt
@@ -1,20 +1,21 @@
-package br.com.fabianafarias.todolist
+package br.com.fabianafarias.todolist.ui
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fabianafarias.todolist.databinding.ActivityMainBinding
-import br.com.fabianafarias.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
 
         insertListeners()
     }
