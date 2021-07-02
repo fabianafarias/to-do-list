c01deddb9caaabf847aee4793ec49bafd694ed42
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-02 19:03:45 -0300

    Excluindo hard code da activity_main e chamando a add_task_activity com o Float Action Button

--- app/src/main/java/br/com/fabianafarias/todolist/MainActivity.kt
+++ app/src/main/java/br/com/fabianafarias/todolist/MainActivity.kt
@@ -1,11 +1,27 @@
 package br.com.fabianafarias.todolist
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fabianafarias.todolist.databinding.ActivityMainBinding
+import br.com.fabianafarias.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
