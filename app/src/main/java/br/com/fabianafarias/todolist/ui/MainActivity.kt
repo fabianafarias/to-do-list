2e05e6608eb6a474793b17217bf509e95db789e4
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-03 22:07:23 -0300

    Adicioanando imagem e mensagem à lista sem tarefas

--- app/src/main/java/br/com/fabianafarias/todolist/ui/MainActivity.kt
+++ app/src/main/java/br/com/fabianafarias/todolist/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import br.com.fabianafarias.todolist.databinding.ActivityMainBinding
 import br.com.fabianafarias.todolist.datasource.TaskDataSource
 
@@ -47,7 +48,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateList() {
-        adapter.submitList(TaskDataSource.getList())
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
     }
 
     companion object {
